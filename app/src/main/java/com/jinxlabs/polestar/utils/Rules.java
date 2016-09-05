34b7b3218f2a0a20a1d8ca3a873f126c211c9022
Udit Mukherjee <uditmukherjee457@gmail.com>
2016-09-06 03:51:17 +0530

    Created the rules class

---
+++ app/src/main/java/com/jinxlabs/polestar/utils/Rules.java
@@ -0,0 +1,14 @@
+package com.jinxlabs.polestar.utils;
+
+/**
+ * Created by uditmukherjee on 06/09/16.
+ */
+public class Rules {
+  public enum NotificationHandling {
+    MUTE, DISMISS, HIDE_AND_SHOW_LATER
+  }
+
+  public enum ExclusionRule {
+    SELECTED, ALL_BUT_SELECTED
+  }
+}
