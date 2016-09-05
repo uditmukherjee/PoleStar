181e4a2910dc654d0c9d0177ba22238658662366
Udit Mukherjee <uditmukherjee457@gmail.com>
2016-09-06 04:57:54 +0530

    build the object graph

--- app/src/main/java/com/jinxlabs/polestar/PoleStarApp.java
+++ app/src/main/java/com/jinxlabs/polestar/PoleStarApp.java
@@ -1,13 +1,30 @@
 package com.jinxlabs.polestar;
 
 import android.app.Application;
-
+import com.jinxlabs.polestar.injectors.components.AppComponent;
+import com.jinxlabs.polestar.injectors.components.DaggerAppComponent;
+import com.jinxlabs.polestar.injectors.modules.AppModule;
 
 /**
  * Created by uditmukherjee on 06/09/16.
  */
 public class PoleStarApp extends Application {
-  @Override public void onCreate() {
+  private AppComponent mAppComponent;
+
+  @Override
+  public void onCreate() {
     super.onCreate();
+    initializeDI();
   }
+
+  private void initializeDI() {
+    mAppComponent = DaggerAppComponent.builder()
+        .appModule(new AppModule(this))
+        .build();
+  }
+
+  public AppComponent getAppComponent() {
+    return mAppComponent;
+  }
+
 }
