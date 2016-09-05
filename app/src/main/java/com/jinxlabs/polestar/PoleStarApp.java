86aa9c923c05aead8b0f41206fa4f5ea358147d8
Udit Mukherjee <uditmukherjee457@gmail.com>
2016-09-06 01:24:33 +0530

    created the partial layout for configs

--- app/src/main/java/com/jinxlabs/polestar/PoleStarApp.java
+++ app/src/main/java/com/jinxlabs/polestar/PoleStarApp.java
@@ -2,6 +2,10 @@ package com.jinxlabs.polestar;
 
 import android.app.Application;
 
+
+/**
+ * Created by uditmukherjee on 06/09/16.
+ */
 public class PoleStarApp extends Application {
   @Override public void onCreate() {
     super.onCreate();
